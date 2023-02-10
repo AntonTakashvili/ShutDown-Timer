fdcca68c05a56c8cfacb6369699cfd7315e5131a
AntonTakashvili <anton.takashvili.1@btu.edu.ge>
2023-02-10 23:36:25 +0300

    Добавьте файлы проекта.

---
+++ ShutDown Timer/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ShutDown_Timer
+{
+    partial class Timer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Timer));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxHour = new System.Windows.Forms.TextBox();
+            this.activatebtn = new System.Windows.Forms.Button();
+            this.deactivatebtn = new System.Windows.Forms.Button();
+            this.textBoxMin = new System.Windows.Forms.TextBox();
+            this.textBoxSec = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Name = "label1";
+            // 
+            // textBoxHour
+            // 
+            this.textBoxHour.ForeColor = System.Drawing.SystemColors.WindowText;
+            resources.ApplyResources(this.textBoxHour, "textBoxHour");
+            this.textBoxHour.Name = "textBoxHour";
+            // 
+            // activatebtn
+            // 
+            resources.ApplyResources(this.activatebtn, "activatebtn");
+            this.activatebtn.Name = "activatebtn";
+            this.activatebtn.UseVisualStyleBackColor = true;
+            this.activatebtn.Click += new System.EventHandler(this.activatebtn_Click);
+            // 
+            // deactivatebtn
+            // 
+            resources.ApplyResources(this.deactivatebtn, "deactivatebtn");
+            this.deactivatebtn.Name = "deactivatebtn";
+            this.deactivatebtn.UseVisualStyleBackColor = true;
+            this.deactivatebtn.Click += new System.EventHandler(this.deactivatebtn_Click);
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.ForeColor = System.Drawing.SystemColors.WindowText;
+            resources.ApplyResources(this.textBoxMin, "textBoxMin");
+            this.textBoxMin.Name = "textBoxMin";
+            // 
+            // textBoxSec
+            // 
+            this.textBoxSec.ForeColor = System.Drawing.SystemColors.WindowText;
+            resources.ApplyResources(this.textBoxSec, "textBoxSec");
+            this.textBoxSec.Name = "textBoxSec";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Name = "label3";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.deactivatebtn);
+            this.panel1.Controls.Add(this.activatebtn);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBoxHour);
+            this.panel1.Controls.Add(this.textBoxSec);
+            this.panel1.Controls.Add(this.textBoxMin);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Name = "label4";
+            // 
+            // Timer
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "Timer";
+            this.ShowIcon = false;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxHour;
+        private System.Windows.Forms.Button activatebtn;
+        private System.Windows.Forms.Button deactivatebtn;
+        private System.Windows.Forms.TextBox textBoxMin;
+        private System.Windows.Forms.TextBox textBoxSec;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ShutDown Timer/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ShutDown_Timer
+{
+    public partial class Timer : Form
+    {
+        public Timer()
+        {
+            InitializeComponent();
+        }
+        //Activation Button
+        private void activatebtn_Click(object sender, EventArgs e)
+        {
+            string HourLabel = (textBoxHour.Text);
+            string MinuteLabel = (textBoxMin.Text);
+            string SecondLabel = (textBoxSec.Text);
+            if (MinuteLabel=="" && SecondLabel =="" && HourLabel!="")
+            {
+                HourTimer(HourLabel);
+            }
+            else if (HourLabel == ""& MinuteLabel!="" & SecondLabel == "")
+            {
+                MinuteTimer(MinuteLabel);
+            }
+            else if (HourLabel==""&MinuteLabel==""&SecondLabel!="")
+            {
+                SecondTimer(SecondLabel);
+            }
+            else if (HourLabel==""&MinuteLabel==""&SecondLabel=="")
+            {
+                MessageBox.Show("Fill any of this field!", "Error");
+            }
+            else
+            {
+                TotalTimer(HourLabel, MinuteLabel, SecondLabel);
+            }
+            //After Clicking clearing Fields
+            textBoxHour.Clear();
+            textBoxMin.Clear();
+            textBoxSec.Clear();
+        }
+        //Deactivation Button
+        private void deactivatebtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("shutdown.exe", "/a");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong!!", "Erroe");
+            }
+        }
+        private void HourTimer(string hour)
+        {
+            int ConvertHour=Convert.ToInt32(hour);
+            int MultiplySecondsToHour = ConvertHour * 3600;
+            try
+            {
+                Process.Start("shutdown.exe", $"/s /t {MultiplySecondsToHour}");
+                MessageBox.Show($"Your PC will turn of in {MultiplySecondsToHour/3600} Hour", "Success");
+            }
+            catch (SyntaxErrorException ex)
+            {
+                MessageBox.Show("Label is Empty!", "Error");
+            }
+        }
+        private void MinuteTimer(string min)
+        {
+            int ConvertHour = Convert.ToInt32(min);
+            int MultiplySecondsToMinute = ConvertHour * 60;
+            try
+            {
+                Process.Start("shutdown.exe", $"/s /t {MultiplySecondsToMinute}");
+                MessageBox.Show($"Your PC will turn of in {MultiplySecondsToMinute / 60} Minute", "Success");
+            }
+            catch (SyntaxErrorException ex)
+            {
+                MessageBox.Show("Label is Empty!", "Error");
+            }
+        }
+        private void SecondTimer(string second)
+        {
+            int ConvertSecond = Convert.ToInt32(second);
+            try
+            {
+                Process.Start("shutdown.exe", $"/s /t {ConvertSecond}");
+                MessageBox.Show($"Your PC will turn of in {ConvertSecond} Second", "Success");
+            }
+            catch (SyntaxErrorException ex)
+            {
+                MessageBox.Show("Label is Empty!", "Error");
+            }
+        }
+        private void TotalTimer(string hour,string min,string second)
+        {
+            int ConvertHour = Convert.ToInt32(hour);
+            int ConvertMin = Convert.ToInt32(min);
+            int ConvertSecond = Convert.ToInt32(second);
+            int MultiplyHourToSecond = ConvertHour * 3600;
+            int MultiplyMinuteToSecond = ConvertMin * 60;
+            try
+            {
+                Process.Start("shutdown.exe", $"/s /t {MultiplyHourToSecond+MultiplyMinuteToSecond+ConvertSecond}");
+                MessageBox.Show($"Your PC will turn of in {MultiplyHourToSecond / 3600} Hour {MultiplyMinuteToSecond/60} Minute {ConvertSecond} Second", "Success");
+            }
+            catch (SyntaxErrorException ex)
+            {
+                MessageBox.Show("Label is Empty!", "Error");
+            }
+        }
+    }
+}
